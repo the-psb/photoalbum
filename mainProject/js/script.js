2c88121fa2a65c2ab6f8af69f14aeb88103fa0f8
Nicholas <41295870+the-psb@users.noreply.github.com>
2023-11-17 19:38:17 +0300

    Add files via upload

---
+++ mainProject/js/script.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+        dynamicBullets: true,
+    },
+    parallax: true,
+    slidesPerView: 3,
+    centeredSlides: false,
+    spaceBetween: 30,
+
+});
